c58aaa805c83cfd768a86df2f06f1c330dcb4469
Lumi Kalt <lumi.mia.kalt@gmail.com>
2022-10-02 20:36:13 +0100

    New home page!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://localhost:3000",
+  site: "https://lumikalt.vercel.app",
   integrations: [mdx(), sitemap()]
 });
